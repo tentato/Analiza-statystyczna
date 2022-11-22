2241e605c6c4c7408fbd7270d0b3dcbf442f9782
tentato <alepaw99@gmail.com>
2022-11-22 19:33:23 +0100

    kruskal

--- main.py
+++ main.py
@@ -6,29 +6,32 @@ from scipy import stats
 
 print(" ")
 
-results_folder = 'results/'
-filename = 'pima/pima.csv'
+# filename = 'pima/pima.csv'
 # filename = 'heart/heart.csv'
-# filename = 'wdbc/wdbc.csv'
+filename = 'wdbc/wdbc.csv'
 
 dataset = pd.read_csv(filename)
 # positive_df = dataset[dataset['Class'] == 1] 
 # negative_df = dataset[dataset['Class'] == 2]
 # positive_df = dataset[dataset['Class'] == "B"] 
 # negative_df = dataset[dataset['Class'] == "M"] 
-positive_df = dataset[dataset['Class'] == "tested_positive"] 
-negative_df = dataset[dataset['Class'] == "tested_negative"]
+# positive_df = dataset[dataset['Class'] == "tested_positive"] 
+# negative_df = dataset[dataset['Class'] == "tested_negative"]
 
 # Shapiro Wilk test
 for idx, col in enumerate(dataset.columns):
     if not col == "Class":
-        positive_attribute_array = positive_df.loc[:, col].to_numpy()
-        negative_attribute_array = negative_df.loc[:, col].to_numpy()
-        positive_shapiro_wilk_test = stats.shapiro(positive_attribute_array)
-        negative_shapiro_wilk_test = stats.shapiro(negative_attribute_array)
-        mannwhitneyu_test = stats.mannwhitneyu(positive_attribute_array, negative_attribute_array)
-        
-        print("Col: ", col, "Positive test: %.5f" % positive_shapiro_wilk_test.pvalue, "Negative test: %.5f" % negative_shapiro_wilk_test.pvalue, "Mannwhitneyu test: %.5f" % mannwhitneyu_test.pvalue)
+        # positive_attribute_array = positive_df.loc[:, col].to_numpy()
+        # negative_attribute_array = negative_df.loc[:, col].to_numpy()
+        g1 = dataset.loc[:,"Radius1"].to_numpy()
+        g2 = dataset.loc[:,"Radius2"].to_numpy()
+        g3 = dataset.loc[:,"Radius3"].to_numpy()
+        # positive_shapiro_wilk_test = stats.shapiro(positive_attribute_array)
+        # negative_shapiro_wilk_test = stats.shapiro(negative_attribute_array)
+        # mannwhitneyu_test = stats.mannwhitneyu(positive_attribute_array, negative_attribute_array)
+        kruskal_test = stats.kruskal(g1, g2, g3)
+        # print("Col: ", col, "Positive test: %.5f" % positive_shapiro_wilk_test.pvalue, "Negative test: %.5f" % negative_shapiro_wilk_test.pvalue, "Mannwhitneyu test: %.5f" % mannwhitneyu_test.pvalue)
+        print(kruskal_test)
 
 print("")
 
